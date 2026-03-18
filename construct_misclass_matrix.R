b520a6fda97055bfdce84f8c17c2b5d5a0f9335c
Rui Hu <89792665+ruihu51@users.noreply.github.com>
2026-03-18 19:00:56 +0800

    add health status misclassification matrices

--- construct_misclass_matrix.R
+++ construct_misclass_matrix.R
@@ -1,5 +1,5 @@
 #########################
-# generate 9 misclassification matrix
+# generate 9 misclassification matrices for smoking intensity
 ########################
 # sym 1 mild
 true_prob <- 0.9
@@ -126,3 +126,126 @@ p_ij.4.3 <- matrix(c(row[1],row[2:6],
                      row[6:1]), nrow = 6, byrow = FALSE)
 check.mc.matrix(list(p_ij.4.3))
 dimnames(p_ij.4.3) <- list(levels(aarp_data$SMOKE_DOSE), levels(aarp_data$SMOKE_DOSE))
+
+# sym 1 mild
+true_prob <- 0.8
+false_prob <- 1 - true_prob
+p_ij.1.1.health <- matrix(c(true_prob, false_prob, 0,
+                            false_prob/2, true_prob, false_prob/2,
+                            0, false_prob, true_prob), nrow = 3, byrow = FALSE)
+p_ij.1.1.health <- build.mc.matrix(p_ij.1.1.health, method = "jlt")
+p_ij.1.1.health <- rbind(p_ij.1.1.health, c(0, 0, 0))
+p_ij.1.1.health <- cbind(p_ij.1.1.health, c(0, 0, 0, 1))
+dimnames(p_ij.1.1.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.1.1.health))
+save(p_ij.1.1.health, file="health_add_data/p_ij.1.1.health.RData")
+
+# sym 1 moderate
+true_prob <- 0.6
+false_prob <- 1 - true_prob
+p_ij.1.2.health <- matrix(c(true_prob, false_prob, 0,
+                            false_prob/2, true_prob, false_prob/2,
+                            0, false_prob, true_prob), nrow = 3, byrow = FALSE)
+p_ij.1.2.health <- build.mc.matrix(p_ij.1.2.health, method = "jlt")
+p_ij.1.2.health <- rbind(p_ij.1.2.health, c(0, 0, 0))
+p_ij.1.2.health <- cbind(p_ij.1.2.health, c(0, 0, 0, 1))
+dimnames(p_ij.1.2.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.1.2.health))
+save(p_ij.1.2.health, file="health_add_data/p_ij.1.2.health.RData")
+
+# sym 1 extreme
+true_prob <- 0.3
+false_prob <- 1 - true_prob
+p_ij.1.3.health <- matrix(c(true_prob, false_prob, 0,
+                            false_prob/2, true_prob, false_prob/2,
+                            0, false_prob, true_prob), nrow = 3, byrow = FALSE)
+p_ij.1.3.health <- build.mc.matrix(p_ij.1.3.health, method = "jlt")
+p_ij.1.3.health <- rbind(p_ij.1.3.health, c(0, 0, 0))
+p_ij.1.3.health <- cbind(p_ij.1.3.health, c(0, 0, 0, 1))
+dimnames(p_ij.1.3.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.1.3.health))
+save(p_ij.1.3.health, file="health_add_data/p_ij.1.3.health.RData")
+
+prop_asym1 <- 1 + 1/2
+
+# asym 1 mild
+true_prob <- 0.8
+false_prob <- 1 - true_prob
+row <- c(true_prob, false_prob*(1/prop_asym1), false_prob*(0.5/prop_asym1))
+p_ij.3.1.health <- matrix(c(row[1], row[2:3],
+                            row[2:1], row[3],
+                            row[3:1]), nrow = 3, byrow = FALSE)
+p_ij.3.1.health <- rbind(p_ij.3.1.health, c(0, 0, 0))
+p_ij.3.1.health <- cbind(p_ij.3.1.health, c(0, 0, 0, 1))
+dimnames(p_ij.3.1.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.3.1.health))
+save(p_ij.3.1.health, file="health_add_data/p_ij.3.1.health.RData")
+
+# asym 1 moderate
+true_prob <- 0.6
+false_prob <- 1 - true_prob
+row <- c(true_prob, false_prob*(1/prop_asym1), false_prob*(0.5/prop_asym1))
+p_ij.3.2.health <- matrix(c(row[1], row[2:3],
+                            row[2:1], row[3],
+                            row[3:1]), nrow = 3, byrow = FALSE)
+p_ij.3.2.health <- rbind(p_ij.3.2.health, c(0, 0, 0))
+p_ij.3.2.health <- cbind(p_ij.3.2.health, c(0, 0, 0, 1))
+dimnames(p_ij.3.2.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.3.2.health))
+save(p_ij.3.2.health, file="health_add_data/p_ij.3.2.health.RData")
+
+# asym 1 extreme
+true_prob <- 0.3
+false_prob <- 1 - true_prob
+row <- c(true_prob, false_prob*(1/prop_asym1), false_prob*(0.5/prop_asym1))
+p_ij.3.3.health <- matrix(c(row[1], row[2:3],
+                            row[2:1], row[3],
+                            row[3:1]), nrow = 3, byrow = FALSE)
+p_ij.3.3.health <- build.mc.matrix(p_ij.3.3.health, method = "jlt")
+p_ij.3.3.health <- rbind(p_ij.3.3.health, c(0, 0, 0))
+p_ij.3.3.health <- cbind(p_ij.3.3.health, c(0, 0, 0, 1))
+dimnames(p_ij.3.3.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.3.3.health))
+save(p_ij.3.3.health, file="health_add_data/p_ij.3.3.health.RData")
+
+prop_asym2 <- 1 + 0.75
+
+# asym 2 mild
+true_prob <- 0.8
+false_prob <- 1 - true_prob
+row <- c(true_prob, false_prob*(1/prop_asym2), false_prob*(0.75/prop_asym2))
+p_ij.4.1.health <- matrix(c(row[1], row[2:3],
+                            row[2:1], row[3],
+                            row[3:1]), nrow = 3, byrow = FALSE)
+p_ij.4.1.health <- rbind(p_ij.4.1.health, c(0, 0, 0))
+p_ij.4.1.health <- cbind(p_ij.4.1.health, c(0, 0, 0, 1))
+dimnames(p_ij.4.1.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.4.1.health))
+save(p_ij.4.1.health, file="health_add_data/p_ij.4.1.health.RData")
+
+# asym 2 moderate
+true_prob <- 0.6
+false_prob <- 1 - true_prob
+row <- c(true_prob, false_prob*(1/prop_asym2), false_prob*(0.75/prop_asym2))
+p_ij.4.2.health <- matrix(c(row[1], row[2:3],
+                            row[2:1], row[3],
+                            row[3:1]), nrow = 3, byrow = FALSE)
+p_ij.4.2.health <- rbind(p_ij.4.2.health, c(0, 0, 0))
+p_ij.4.2.health <- cbind(p_ij.4.2.health, c(0, 0, 0, 1))
+dimnames(p_ij.4.2.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.4.2.health))
+save(p_ij.4.2.health, file="health_add_data/p_ij.4.2.health.RData")
+
+# asym 2 extreme
+true_prob <- 0.3
+false_prob <- 1 - true_prob
+row <- c(true_prob, false_prob*(1/prop_asym2), false_prob*(0.75/prop_asym2))
+p_ij.4.3.health <- matrix(c(row[1], row[2:3],
+                            row[2:1], row[3],
+                            row[3:1]), nrow = 3, byrow = FALSE)
+p_ij.4.3.health <- build.mc.matrix(p_ij.4.3.health, method = "jlt")
+p_ij.4.3.health <- rbind(p_ij.4.3.health, c(0, 0, 0))
+p_ij.4.3.health <- cbind(p_ij.4.3.health, c(0, 0, 0, 1))
+dimnames(p_ij.4.3.health) <- list(health_levels, health_levels)
+check.mc.matrix(list(p_ij.4.3.health))
+save(p_ij.4.3.health, file="health_add_data/p_ij.4.3.health.RData")
